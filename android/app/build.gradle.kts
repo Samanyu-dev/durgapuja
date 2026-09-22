1337c74b7c076223bd2dbb2bfd7cbc898f37d5d3
Sudhanva B H <bhsudhanva@gmail.com>
2026-09-22 22:18:50 +0530

    chore: add shared debug keystore for team

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,6 +34,12 @@ android {
     }
 
     signingConfigs {
+        getByName("debug") {
+            storeFile = file("debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
         create("release") {
             val keystorePropertiesFile = rootProject.file("key.properties")
             if (keystorePropertiesFile.exists()) {
